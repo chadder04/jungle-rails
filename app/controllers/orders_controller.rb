432e67b03b5c522f227a27ceb87231031ff14660
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-12 13:55:10 -0700

    Defined email_order method to send mail using OrderMailer, updated KVs details to use my own

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
+      email_order order
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
@@ -30,7 +31,7 @@ class OrdersController < ApplicationController
     Stripe::Charge.create(
       source:      params[:stripeToken],
       amount:      cart_total, # in cents
-      description: "Khurram Virani's Jungle Order",
+      description: "Chad Garrett's Jungle Order",
       currency:    'cad'
     )
   end
@@ -56,6 +57,10 @@ class OrdersController < ApplicationController
     order
   end
 
+  def email_order(order) 
+    OrderMailer.order_email(order).deliver_now
+  end
+  
   # returns total in cents not dollars (stripe uses cents as well)
   def cart_total
     total = 0
