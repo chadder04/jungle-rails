788bf14f6d3d789ab1a076a36fe78d2d76217923
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-12 15:15:32 -0700

    Defined helper method for authoize that uses basic HTTP auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
+  helper_method :authorize
+
   private
 
   def cart
