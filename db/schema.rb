a6c0396e9c9a5eca3f61152414f0e287e7f9aa75
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-14 21:56:42 -0700

    Schema updated as per new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012224302) do
+ActiveRecord::Schema.define(version: 20171013214857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 20171012224302) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
