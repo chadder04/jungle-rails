4765c27bf67ef421ed55dc6a54b92429e10ea58d
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-17 16:33:05 -0700

    Added more validation methods and authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,18 @@ class User < ActiveRecord::Base
     has_many :reviews
 
     validates :name, presence: true
-    validates :email, uniqueness: true
+    validates :email, uniqueness: true, presence: true
+    validates :password, presence: true, length: { minimum: 3 }
+    validates :password_confirmation, presence: true, length: { minimum: 3 }
     has_secure_password
+
+    def self.authenticate_with_credentials(email, password)
+        @user = User.find_by_email(email.to_s.downcase.strip)
+        if @user && @user.authenticate(password)
+          @user
+        else
+          nil
+        end
+    end
     
 end
